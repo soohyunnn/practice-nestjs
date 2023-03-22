8b882f6eefb169a1bfbd1b5b64eb0a38c69a0d79
soohyunnn <sh.choi@cbcm-co.com>
2023-03-22 23:15:02 +0900

    nestJS 1,2,3장 공부

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BoardModule } from './board/board.module';
 import { ApiController } from './api/api.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BoardModule, UsersModule],
-  controllers: [ApiController, AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [ApiController],
+  providers: [],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,59 +16,87 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {VerifyEmailDto} from "./dto/verify-email.dto";
+import {UserLoginDto} from "./dto/user-login.dto";
+import {UserInfo} from "./interface/user-login-interface";
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {
+  }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    const { name, email } = createUserDto;
-
-    return `유저를 생성했습니다. 이름: ${name}, 이메일: ${email}`;
-    // return this.usersService.create(createUserDto);
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
   }
 
-  @Get()
-  findAll(@Res() res) {
-    const users = this.usersService.findAll();
-    return res.status(200).send(users);
-  }
-  @Header('Custom', 'Test Header')
-  @Redirect('https://nestjs.com', 301)
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    if (+id < 1) {
-      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
-    }
-
-    return this.usersService.findOne(+id);
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
   }
 
-  @HttpCode(202)
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
   }
 
-  @Get('redirect/docs')
-  @Redirect('https://docs.nestjs.com', 302)
-  getDocs(@Query('version') version) {
-    if (version && version === '5') {
-      return { url: 'https://docs.nestjs.com/v5/' };
-    }
-  }
 
-  @Delete(':userId/memo/:memoId')
-  deleteUserMemo(
-    @Param('userId') userId: string,
-    @Param('memoId') memoId: string,
-  ) {
-    return `userId: ${userId}, memoId: ${memoId}`;
-  }
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   const { name, email } = createUserDto;
+  //
+  //   return `유저를 생성했습니다. 이름: ${name}, 이메일: ${email}`;
+  //   // return this.usersService.create(createUserDto);
+  // }
+  //
+  // @Get()
+  // findAll(@Res() res) {
+  //   const users = this.usersService.findAll();
+  //   return res.status(200).send(users);
+  // }
+  // @Header('Custom', 'Test Header')
+  // @Redirect('https://nestjs.com', 301)
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   if (+id < 1) {
+  //     throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+  //   }
+  //
+  //   return this.usersService.findOne(+id);
+  // }
+  //
+  // @HttpCode(202)
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
+  //
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
+  //
+  // @Get('redirect/docs')
+  // @Redirect('https://docs.nestjs.com', 302)
+  // getDocs(@Query('version') version) {
+  //   if (version && version === '5') {
+  //     return { url: 'https://docs.nestjs.com/v5/' };
+  //   }
+  // }
+  //
+  // @Delete(':userId/memo/:memoId')
+  // deleteUserMemo(
+  //   @Param('userId') userId: string,
+  //   @Param('memoId') memoId: string,
+  // ) {
+  //   return `userId: ${userId}, memoId: ${memoId}`;
+  // }
 }
