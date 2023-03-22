780966a96d8481362e9764eae6714a0665edf5d8
최수현 <pia@lane4.ai>
2023-03-22 15:06:40 +0900

    nest study first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
 import { BoardModule } from './board/board.module';
 import { ApiController } from './api/api.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BoardModule],
-  controllers: [AppController, UsersController, ApiController],
+  imports: [BoardModule, UsersModule],
+  controllers: [ApiController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BoardService } from './board.service';
+import { UsersService } from './users.service';
 
-describe('BoardService', () => {
-  let service: BoardService;
+describe('UsersService', () => {
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardService],
+      providers: [UsersService],
     }).compile();
 
-    service = module.get<BoardService>(BoardService);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
